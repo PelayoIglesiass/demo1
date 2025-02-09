2c3a3dd9825fa2e805e5df9dde96b93d9eb73fa5
Pelayo Iglesias Manzano <uo266600@uniovi.es>
2025-02-09 17:11:28 +0100

    cambio 3 en conflicto

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -6,7 +6,7 @@ package demo;
 public class Demo {
 	public static void main(String[] args){
 		System.out.println("linea 1");
-		System.out.println("linea 2");
+		System.out.println("cambio 3 en conflicto");
 		System.out.println("linea 3");
 	}
 }
